3634c0aebbfb2f627326a7292a1d6fb2c3d1728c
raul <192793@iberopuebla.mx>
2024-02-11 21:29:21 -0600

    api, dbcreation needs work, created local on psql

--- app.py
+++ app.py
@@ -1,38 +1,46 @@
-from flask import Flask, render_template, request, flash, redirect, url_for
-import sys
-from form import MyForm
-from transferencia import cuenta
+from flask import Flask, request, jsonify
+from transferencia import cuenta, realizar_deposito, realizar_retiro, realizar_transferencia
 
-cuentas={123:cuenta(123, 1000),124:cuenta(124, 100), 125:cuenta(125, 100)}
+cuentas={"C123":cuenta("C123", 1000),"C124":cuenta("C124", 100), "C125":cuenta("C125", 100)}
 
 app = Flask(__name__)
-# app.config['SECRET_KEY'] = 'your_secret_key'  # Needed for CSRF protection
+@app.route('/deposito', methods=['POST'])
+def deposito():
+    data = request.get_json()
+    no_cuenta = data.get('no_cuenta')
+    cantidad = data.get('cantidad')
 
-# @app.route('/form', methods=['GET', 'POST'])
-# def form():
-#     form = MyForm()
-#     if form.validate_on_submit():
-#         # Process valid form data (e.g., save to database)
-#         flash('Registration Successful!')
-#         return redirect(url_for('form'))
-#     return render_template('form.html', form=form)
+    if no_cuenta in cuentas and cantidad > 0:
+        realizar_deposito(cuentas[no_cuenta], cantidad)
+        return jsonify({"message": "Deposito exitoso", "balance": cuentas[no_cuenta].saldo}), 200
+    else:
+        return jsonify({"message": "Error"}), 400
 
+@app.route('/retiro', methods=['POST'])
+def retiro():
+    data = request.get_json()
+    no_cuenta = data.get('no_cuenta')
+    cantidad = data.get('cantidad')
 
-
-@app.route("/",methods=['GET', 'POST'])
-def index():
-    global cuentas
-    if request.method == 'POST':
-        if request.form['transactionType']=='deposito':
-            cuentas[123].deposito(int(request.form['cantidad'])) 
-        elif request.form['transactionType']=='retiro':
-            cuentas[123].retiro(int(request.form['cantidad']))
-        elif request.form['transactionType']=='transferencia':
-            cuentas[123].transferencia(cuentas[int(request.form['destino'])], int(request.form['cantidad']))
-            
-    return render_template('index.html', saldo = cuentas[123].saldo)
-
+    if no_cuenta in cuentas and cantidad <= cuentas[no_cuenta].saldo:
+        realizar_retiro(cuentas[no_cuenta], cantidad)
+        return jsonify({"message": "Retiro exitoso", "balance": cuentas[no_cuenta].saldo}), 200
+    else:
+        return jsonify({"message": "Error"}), 400
     
+@app.route('/transferencia', methods=['POST'])
+def transferencia():
+    data = request.get_json()
+    no_cuenta = data.get('no_cuenta')
+    cantidad = data.get('cantidad')
+    destino = data.get('destino')
 
+    if no_cuenta in cuentas and cantidad <= cuentas[no_cuenta].saldo and destino in cuentas:
+        realizar_transferencia(cuentas[no_cuenta], cuentas[destino], cantidad)
+        return jsonify({"message": "transferencia exitosa", "balance": cuentas[no_cuenta].saldo}), 200
+    else:
+        return jsonify({"message": "Error"}), 400
+    
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug =True )
+    
