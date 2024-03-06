11447e99766bf6bac08c9ad9b3f19aeae40e0e94
raul <192793@iberopuebla.mx>
2024-03-05 19:26:19 -0600

    app conexion db

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 from flask import Flask, request, jsonify
-from transferencia import cuenta, realizar_deposito, realizar_retiro, realizar_transferencia
 
-cuentas={"C123":cuenta("C123", 1000),"C124":cuenta("C124", 100), "C125":cuenta("C125", 100)}
+from transferencia_DB import connect, deposit, withdraw, add_account, transfer
 
 app = Flask(__name__)
 @app.route('/deposito', methods=['POST'])
@@ -9,24 +8,18 @@ def deposito():
     data = request.get_json()
     no_cuenta = data.get('no_cuenta')
     cantidad = data.get('cantidad')
-
-    if no_cuenta in cuentas and cantidad > 0:
-        realizar_deposito(cuentas[no_cuenta], cantidad)
-        return jsonify({"message": "Deposito exitoso", "balance": cuentas[no_cuenta].saldo}), 200
-    else:
-        return jsonify({"message": "Error"}), 400
+    connection = connect()
+    deposit(connection, no_cuenta, cantidad)
+    
 
 @app.route('/retiro', methods=['POST'])
 def retiro():
     data = request.get_json()
     no_cuenta = data.get('no_cuenta')
     cantidad = data.get('cantidad')
-
-    if no_cuenta in cuentas and cantidad <= cuentas[no_cuenta].saldo:
-        realizar_retiro(cuentas[no_cuenta], cantidad)
-        return jsonify({"message": "Retiro exitoso", "balance": cuentas[no_cuenta].saldo}), 200
-    else:
-        return jsonify({"message": "Error"}), 400
+    connection = connect()
+    
+    withdraw(connection, no_cuenta, withdraw)
     
 @app.route('/transferencia', methods=['POST'])
 def transferencia():
@@ -34,12 +27,17 @@ def transferencia():
     no_cuenta = data.get('no_cuenta')
     cantidad = data.get('cantidad')
     destino = data.get('destino')
-
-    if no_cuenta in cuentas and cantidad <= cuentas[no_cuenta].saldo and destino in cuentas:
-        realizar_transferencia(cuentas[no_cuenta], cuentas[destino], cantidad)
-        return jsonify({"message": "transferencia exitosa", "balance": cuentas[no_cuenta].saldo}), 200
-    else:
-        return jsonify({"message": "Error"}), 400
+    connection = connect()
+    
+    transfer(connection, no_cuenta, destino, cantidad)
+    
+@app.route('/nueva_cuenta', method =['POST'])
+def nueva_cuenta():
+    data = request.get_json()
+    cantidad = data.get('cantidad')
+    connection = connect()
+    
+    add_account(connection, cantidad)
     
 if __name__ == '__main__':
     app.run(debug =True )
