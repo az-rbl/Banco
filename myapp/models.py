bb1b84b840378a8fc9175bd89ac8fd175c368436
az-rbl <bl.raul@outlook.com>
2024-03-30 16:07:12 -0600

    new_account

--- myapp/models.py
+++ myapp/models.py
@@ -4,6 +4,6 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(50))
 
-class Account(db.Model):
+class Accounts(db.Model):
     account_id = db.Column(db.Integer, primary_key=True)
     balance = db.Column(db.Numeric(12,2))
--- myapp/routes.py
+++ myapp/routes.py
@@ -1,16 +1,28 @@
-from flask import Blueprint, redirect, url_for
+from flask import Blueprint, redirect, url_for, request
 
 from .extensions import db
-from .models import Account
+from .models import Accounts
 from .models import User
 
 main = Blueprint('main', __name__)
 
 @main.route('/')
 def index():
-    accounts = Account.query.all()
+    accounts = Accounts.query.all()
     account_list_html = [f"<li>{ account.balance }</li>" for account in accounts]
     return f"<ul>{''.join(account_list_html)}</ul>"
+
+
+@main.route('/new_account', methods=['POST'])
+def new_account():
+    data = request.get_json()
+    amount = data.get('amount')
+    db.session.add(Accounts(balance=amount))
+    db.session.commit()
+    return redirect(url_for("main.index"))
+
+
+
 
 @main.route('/add/<username>')
 def add_user(username):
